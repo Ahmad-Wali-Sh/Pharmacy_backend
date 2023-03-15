aa292d2525c91fec37eb271f0724681e3cb84562
Ahmad Wali <ahmadwalisharifybtc@gmail.com>
2023-03-15 22:29:09 +0430

    Models Changed, Import Export Applied

--- medician/admin.py
+++ medician/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 
 from .models import Pharm_Group, Medician, Kind, Country, Unit
 
-admin.site.register(Pharm_Group)
-admin.site.register(Medician)
-admin.site.register(Kind)
-admin.site.register(Country)
-admin.site.register(Unit)
+class ImportAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    pass
+
+admin.site.register(Pharm_Group, ImportAdmin)
+admin.site.register(Medician, ImportAdmin)
+admin.site.register(Kind, ImportAdmin)
+admin.site.register(Country, ImportAdmin)
+admin.site.register(Unit, ImportAdmin)
 
 # Register your models here.
