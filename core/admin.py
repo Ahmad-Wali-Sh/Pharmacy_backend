dbb63bbb7a11153a000e7601a8abe917c3e7ba10
Ahmad Wali <ahmadwalisharifybtc@gmail.com>
2023-03-26 23:07:08 +0430

    Prescription Evolved

--- core/admin.py
+++ core/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 
 from core.models import PharmGroup, Medician, Kind, Country, Unit, Prescription, PharmCompany, \
-    Store, Currency, Entrance, EntranceThrough, PaymentMethod, FinalRegister
+    Store, Currency, Entrance, EntranceThrough, PaymentMethod, FinalRegister, Department, DoctorName, PersonalName, PrescriptionThrough
 
 
 class ImportAdmin(ImportExportModelAdmin, admin.ModelAdmin):
@@ -28,5 +28,9 @@ admin.site.register(Currency, ImportAdmin)
 admin.site.register(EntranceThrough, EntranceThrougheAdmin)
 admin.site.register(PaymentMethod, ImportAdmin)
 admin.site.register(FinalRegister, ImportAdmin)
+admin.site.register(Department, ImportAdmin)
+admin.site.register(DoctorName, ImportAdmin)
+admin.site.register(PersonalName, ImportAdmin)
+admin.site.register(PrescriptionThrough, ImportAdmin)
 
 # Register your models here.
