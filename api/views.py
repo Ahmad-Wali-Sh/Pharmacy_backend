40c2f938cf0e7dbad7f7d2059ed345a530584f6d
Ahmad Wali <ahmadwalisharifybtc@gmail.com>
2023-04-18 10:24:09 +0430

    Search Factor Ivolved

--- api/views.py
+++ api/views.py
@@ -4,8 +4,8 @@ from .serializers import PharmGroupSeralizer, MedicianSeralizer, PharmCompanySer
 from rest_framework.pagination import PageNumberPagination
 from core.models import PharmGroup, Medician, Kind, Country, Unit, Prescription, PharmCompany, \
     Store, Currency, Entrance, EntranceThrough, PaymentMethod, FinalRegister, Department, DoctorName, PatientName, PrescriptionThrough
-from rest_framework import viewsets
-from django_filters import rest_framework as filters
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class StandardResultsSetPagination(PageNumberPagination):
@@ -17,66 +17,85 @@ class StandardResultsSetPagination(PageNumberPagination):
 class MedicianView(viewsets.ModelViewSet):
     queryset = Medician.objects.all()
     serializer_class = MedicianSeralizer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ('brand_name',)
+    search_fields = ['brand_name']
+
 
 class StoreView(viewsets.ModelViewSet):
     queryset = Store.objects.all()
     serializer_class = StoreSerializer
 
+
 class FinalRegisterView(viewsets.ModelViewSet):
     queryset = FinalRegister.objects.all()
     serializer_class = FinalRegisterSerializer
+
 
 class PrescriptionThroughView(viewsets.ModelViewSet):
     queryset = PrescriptionThrough.objects.all()
     serializer_class = PrescriptionThroughSerializer
 
+
 class PatientNameView(viewsets.ModelViewSet):
     queryset = PatientName.objects.all()
     serializer_class = PatientNameSerializer
+
 
 class DoctorNameView(viewsets.ModelViewSet):
     queryset = DoctorName.objects.all()
     serializer_class = DoctorNameSerializer
 
+
 class DepartmentView(viewsets.ModelViewSet):
     queryset = Department.objects.all()
     serializer_class = DepartmentSerializer
+
 
 class CurrencyView(viewsets.ModelViewSet):
     queryset = Currency.objects.all()
     serializer_class = CurrencySerializer
 
+
 class PaymentMethodView(viewsets.ModelViewSet):
     queryset = PaymentMethod.objects.all()
     serializer_class = PaymentMethodSerializer
+
 
 class KindView(viewsets.ModelViewSet):
     queryset = Kind.objects.all()
     serializer_class = KindSerializer
 
+
 class CountryView(viewsets.ModelViewSet):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
+
 
 class PharmGroupView(viewsets.ModelViewSet):
     queryset = PharmGroup.objects.all()
     serializer_class = PharmGroupSeralizer
 
+
 class PrescriptionView(viewsets.ModelViewSet):
     queryset = Prescription.objects.all()
     serializer_class = PrescriptionSerializer
+
 
 class UnitView(viewsets.ModelViewSet):
     queryset = Unit.objects.all()
     serializer_class = UnitSeralizer
 
+
 class PharmCompanyView(viewsets.ModelViewSet):
     queryset = PharmCompany.objects.all()
     serializer_class = PharmCompanySeralizer
 
+
 class EntranceView(viewsets.ModelViewSet):
     queryset = Entrance.objects.all()
     serializer_class = EntranceSerializer
+
 
 class EntranceThroughView(viewsets.ModelViewSet):
     queryset = EntranceThrough.objects.all()
