ca36d840c0d46e9bb918297ad7fab2c110c493a9
Ahmad Wali <ahmadwalisharifybtc@gmail.com>
2023-05-18 01:27:42 +0430

    Filtering Range for Prescription

--- api/views.py
+++ api/views.py
@@ -123,11 +123,16 @@ class PharmGroupView(viewsets.ModelViewSet):
     ordering = ['id',]
     permission_classes = [D7896DjangoModelPermissions]
 
+class PrescriptionFilterView(django_filters.FilterSet):
+    created = django_filters.DateTimeFromToRangeFilter() 
+    class Meta:
+        model = Prescription
+        fields =['prescription_number','department', 'created', 'name', 'doctor', 'prescription_number']
 
 class PrescriptionView(viewsets.ModelViewSet):
     queryset = Prescription.objects.all()
     serializer_class = PrescriptionSerializer
-    filterset_fields = ['prescription_number',]
+    filterset_class = PrescriptionFilterView
     permission_classes = [D7896DjangoModelPermissions]
 
 
