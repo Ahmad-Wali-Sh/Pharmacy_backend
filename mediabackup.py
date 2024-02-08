209e0c446ae69fb6be9550446c1ec0f4d5971931
Ahmad Wali <ahmadwalisharifybtc@gmail.com>
2024-02-08 15:03:15 +0430

    mediabackup evolved.

--- mediabackup.py
+++ mediabackup.py
@@ -10,21 +10,16 @@ def zip_folder(source_folder, destination_zip):
             print(f"Source folder '{source_folder}' does not exist.")
             return
 
-        # Check if destination zip already exists
+        # Remove existing destination zip file if it exists
         if os.path.exists(destination_zip):
-            overwrite = input(f"Destination zip '{destination_zip}' already exists. Overwrite? (y/n): ")
-            if overwrite.lower() != 'y':
-                print("Zip operation aborted.")
-                return
-            else:
-                os.remove(destination_zip)  # Remove existing zip file
+            os.remove(destination_zip)
 
         # Create a zip file and add contents of the source folder
         with zipfile.ZipFile(destination_zip, 'w') as zipf:
             for root, dirs, files in os.walk(source_folder):
                 for file in files:
                     file_path = os.path.join(root, file)
-                    zipf.write(file_path, os.path.relpath(file_path, source_folder + '.rar'))
+                    zipf.write(file_path, os.path.relpath(file_path, source_folder))
 
         print(f"Folder '{source_folder}' zipped to '{destination_zip}' successfully.")
     except Exception as e:
