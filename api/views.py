2e14d70d0cfc315cbcdd2def3c9e9b1198b37cc1
Ahmad Wali Sharify <ahmadwalisharifybtc@gmail.com>
2024-06-10 14:42:03 +0430

    pagination changes to include pages and more...

--- api/views.py
+++ api/views.py
@@ -567,6 +567,15 @@ class PrescriptionPagination(PageNumberPagination):
     page_size_query_param = 'page_size'
     max_page_size = 100
     
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'current_page': self.page.number,
+            'total_pages': self.page.paginator.num_pages,
+            'results': data
+        })
     
 class PrescriptionPaginatedView(viewsets.ModelViewSet):
     queryset = Prescription.objects.all().order_by("id")
